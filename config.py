7d9d92eac1bf618bc7df1e5c35f47cb5b11df8ad
zacsinger <55707744+zacharysinger@users.noreply.github.com>
2020-04-23 13:02:34 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+# help from: https://hackersandslackers.com/managing-user-session-variables-with-flask-sessions-and-redis/
+
+from os import environ, path
+from dotenv import load_dotenv
+import redis
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Set Flask configuration variables from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
+
+    # Flask-Session
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = redis.from_url('redis://localhost:6379')
+
+    # Flask-Assets
+    LESS_BIN = environ.get('LESS_BIN')
+    ASSETS_DEBUG = False
+    LESS_RUN_IN_DEBUG = False
+
+    # Static Assets
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = True
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
