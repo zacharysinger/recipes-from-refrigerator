4837f70f1b47e7c4b74a3438747b3d5f08c5885a
zacsinger <55707744+zacharysinger@users.noreply.github.com>
2020-05-19 10:41:46 -0700

    Update auto_slide.js

--- static/scripts/auto_slide.js
+++ static/scripts/auto_slide.js
@@ -11,7 +11,7 @@ function cycleBackgrounds() {
         // Hide the previous image.
         $imageEls.eq(index - 1).removeClass('show');
 
-        }, 5000);  // the time
+        }, 4500);  // the time
     }
 
     $(function () {
